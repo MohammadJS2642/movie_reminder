da321d9215a7488720c450554ad8cd9ccf5f8c21
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-20 20:40:03 +0430

    DataAnnotation key added

--- backend/SignupAndLoginDLL/Model/UsersModel.cs
+++ backend/SignupAndLoginDLL/Model/UsersModel.cs
@@ -1,8 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SignupAndLoginDLL.Model
 {
     public class UsersModel
     {
+        [Key]
         public int ID { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
