0339009939d342cdd4257184fe5cfd08de6d23a0
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-20 13:46:53 +0430

    UsersModel for create a table with same name in MovieReminderAPI created

--- backend/SignupAndLoginDLL/Model/UsersModel.cs
+++ backend/SignupAndLoginDLL/Model/UsersModel.cs
@@ -3,10 +3,11 @@ namespace SignupAndLoginDLL.Model
 {
     public class UsersModel
     {
-        public int UserID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public int Age { get; set; }
         public DateTime BirthDate { get; set; }
     }
