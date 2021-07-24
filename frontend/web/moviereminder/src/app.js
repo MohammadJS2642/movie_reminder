da1aa848b3f18e2398f138d4ffef9e1ee6b495ba
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-24 22:10:10 +0430

    appbar with minimum options created

--- frontend/web/moviereminder/src/app.js
+++ frontend/web/moviereminder/src/app.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { AppBar } from '@material-ui/core';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function App() {
-
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" align="center">
+            Movie Reminder
+          </Typography>
+          <Button color="inherit">Login/Signup</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default App;
