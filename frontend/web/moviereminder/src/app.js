8a93925b481536e17c5572d405712e214e9062f7
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-07-30 23:49:32 +0430

    use headercomponent to this app.js and appStyle.css for common style for all components

--- frontend/web/moviereminder/src/app.js
+++ frontend/web/moviereminder/src/app.js
@@ -1,32 +1,11 @@
 import React from "react";
-import {
-  Typography,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Button,
-  SwipeableDrawer,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import HeaderBar from "./header";
+import "./appStyle.css";
 
 function App() {
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            onProgress={searcher}
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h4" align="center">
-            Movie Reminder
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <HeaderBar />
     </>
   );
 }
