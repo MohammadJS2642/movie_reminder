98f5797817965a2f04cf6e7e5923442c744bd87d
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-03-04 18:13:52 +0330

    db first models, controller created with DBContext

---
+++ src/MovieReminder/Controllers/MovieReminderController.cs
@@ -0,0 +1,123 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieReminder.DBContext;
+using MovieReminder.Models;
+
+namespace MovieReminder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieReminderController : ControllerBase
+    {
+        private readonly MovieReminderDbContext _context;
+
+        public MovieReminderController(MovieReminderDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MovieReminder
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/MovieReminder/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(Guid id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // PUT: api/MovieReminder/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(Guid id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MovieReminder
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MovieExists(movie.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/MovieReminder/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(Guid id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(Guid id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/MovieReminder/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieReminder.Models
+{
+    public partial class Movie
+    {
+        public Guid Id { get; set; }
+        public string MovieName { get; set; } = null!;
+        public DateTime? ReleaseDate { get; set; }
+        public string? CreatedUsername { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
