4e4c185f1e5f2e5e0d8b9b07a41afac417252824
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-03-04 21:17:22 +0330

    DbContext Added

--- src/UserAuth/Program.cs
+++ src/UserAuth/Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MovieReminderAuthenticationDbContext>(
+    options => options.UseSqlServer("movieReminderAuthentication")
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
