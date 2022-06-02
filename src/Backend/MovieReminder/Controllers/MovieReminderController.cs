526dd7937bc16719c0b1b6de0bcc4647e85b3f54
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-06-02 20:17:57 +0430

    some not needed files deleted

--- src/Backend/MovieReminder/Controllers/MovieReminderController.cs
+++ src/Backend/MovieReminder/Controllers/MovieReminderController.cs
@@ -1,12 +1,4 @@
-#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MovieReminder.DBContext;
 using MovieReminder.Models;
 
 namespace MovieReminder.Controllers
