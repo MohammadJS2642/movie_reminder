00f9127b32d9f6bfc6badad064cff2f52d5c8a41
MohammadJS2642 <mohammadjs2642@gmail.com>
2022-09-08 16:13:17 +0430

    project with pring boot created

---
+++ moviereminder/src/main/java/reminder/movie/moviereminder/MoviereminderApplication.java
@@ -0,0 +1,13 @@
+package reminder.movie.moviereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviereminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviereminderApplication.class, args);
+	}
+
+}
---
+++ moviereminder/src/test/java/reminder/movie/moviereminder/MoviereminderApplicationTests.java
@@ -0,0 +1,13 @@
+package reminder.movie.moviereminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
